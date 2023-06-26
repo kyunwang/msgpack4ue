6b2da50447cdea80c4956a99026469373ce2ce00
Kevin Wang <me@kyunwang.nl>
2023-06-26 15:01:52 +0100

    Feature/enhancement and array support (#1)
    * Add MQTTUtilities plugin for example
    
    Plugin contains adjustment to return TArray<uint8> instead of only FString (Windows only)
    
    * Support Arrays, null, better Objects - Handling failures
    
    * Improve Example along with MQTT usage
    
    - Example along with decoding Arrays and Objects
    - Define enums for topic handling
    
    * Update readme
    
    * Add missing using
    
    * Add edgecase recompile step
    
    * Update mqtt plugin

--- Plugins/msgpack4ue/Source/msgpack4ue/Private/MSGPackUtilitiesBPL.cpp
+++ Plugins/msgpack4ue/Source/msgpack4ue/Private/MSGPackUtilitiesBPL.cpp
@@ -1,33 +1,236 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MSGPackUtilitiesBPL.h"
-// import missing header
-// #include "JsonUtilities.h"
 #include "JsonObjectWrapper.h"
-#include "Templates/SharedPointer.h"
+#include "JsonUtilities.h"
+
+#include "Dom/JsonObject.h"
+#include "Misc/DefaultValueHelper.h"
+#include "Misc/Variant.h"
 #include "Serialization/JsonReader.h"
 #include "Serialization/JsonSerializer.h"
+#include "Templates/SharedPointer.h"
 
+#include "json/json.hpp"
+
+// Helper function to convert a string to a number sfely
+// int32 UMSGPackUtilitiesBPL::SafeStringToInt(const FString& InString)
+// {
+// 	// if (FDefaultValueHelper::IsStringValidInteger(InString))
+// 	if (InString.IsNumeric())
+// 	{
+// 		int32 Result;
+// 		TTypeFromString<int32>::FromString(Result, *InString);
+// 		return Result;
+// 		// return FCString::Atoi(*InString);
+// 	}
+
+// 	UE_LOG(LogTemp, Error, TEXT("Conversion to integer failed for string: %s"), *InString);
+// 	return 0; // Value indicating failure
+// }
+
+// Helper function to convert a string to a float safely
+// float UMSGPackUtilitiesBPL::SafeStringToFloat(const FString& InString)
+// {
+// 	if (FDefaultValueHelper::IsStringValidFloat(InString))
+// 	{
+// 		float Result;
+// 		TTypeFromString<float>::FromString(Result, *InString);
+// 		return Result;
+// 		// return FCString::Atof(*InString);
+// 	}
+// 	UE_LOG(LogTemp, Error, TEXT("Conversion to float failed for string: %s"), *InString);
+// 	return 0.0f; // Value indicating failure
+// }
+
+// nlohmann::json UMSGPackUtilitiesBPL::SafeJsonParse(const TArray<uint8>& InBuffer, bool& OutbIsSuccessful)
+// {
+// 	UE_LOG(LogTemp, Warning, TEXT("Starting method SafeJsonParse..."));
+
+// 	OutbIsSuccessful = false;
+
+// 	// Convert TArray<uint8> to string for inspection
+// 	FString str(UTF8_TO_TCHAR(reinterpret_cast<const char*>(InBuffer.GetData())));
+// 	// UE_LOG(LogTemp, Warning, TEXT("Buffer: %s"), *str);
+
+// 	if (str.IsEmpty()) {
+// 		UE_LOG(LogTemp, Error, TEXT("Error: Buffer is empty: %s"), *str);
+// 		return nlohmann::json();
+// 	}
+// 	else {
+// 		nlohmann::json JsonResult;
+// 		nlohmann::json result;
+
+// 		UE_LOG(LogTemp, Warning, TEXT("Startinfg JSON parsing... %s"), *str);
+
+// 		// Convert FString to std::String for parsing
+// 		// std::string stdStr(TCHAR_TO_UTF8(*str));
+// 		// auto errorHandler = [&](int /*unused*/, std::string_view /*unused*/, nlohmann::json::parse_error& ex) {
+// 		// 	UE_LOG(LogTemp, Error, TEXT("JSON parsing failed: %s"), UTF8_TO_TCHAR(ex.what()));
+// 		// 	OutbIsSuccessful = false;
+// 		// 	// Additional error handling if needed
+// 		// 	return nlohmann::json(nullptr);  // Return a null JSON object
+// 		// };
+
+// 		// std::string stdStr(TCHAR_TO_UTF8(*str));
+
+// 		// nlohmann::json JsonResult = nlohmann::json::parse(stdStr, nullptr, errorHandler);
+
+// 		auto errorHandler = [&](int /*unused*/, std::string_view /*unused*/, nlohmann::json::parse_error& ex)
+// 			{
+// 				UE_LOG(LogTemp, Warning, TEXT("JSON parsing failed: %s"), *FString(ex.what()));
+// 				return false;
+// 			};
+
+// 		// Convert FString to std::string for parse function
+// 		std::string stdStr(TCHAR_TO_UTF8(*str));
+
+// 		if (nlohmann::json::accept(stdStr)) {
+// 			UE_LOG(LogTemp, Warning, TEXT("Wants to parse"));
+// 			// Cannot use parse - crashed without logging
+// 			// return nlohmann::json::parse(stdStr);
+// 		}
+// 		else
+// 			// result = nlohmann::json::parse(stdStr.data(), stdStr.data() + stdStr.size(), nullptr, errorHandler);
+
+// 			// nlohmann::json::parser_callback_t cb = [&](const char* buffer, std::size_t length, bool /*unused*/) -> bool {
+// 			// 	stdStr.assign(buffer, length);
+// 			// 	return true;
+// 			// };
+
+// 			// std::function<bool(int, nlohmann::json::detail::parse_event_t, nlohmann::json&)> callback = cb;
+
+// 			// nlohmann::json JsonResult = nlohmann::json::parse(nlohmann::json::span_input_adapter<std::string>(stdStr), nullptr, errorHandler, callback);
+
+// 			// return nlohmann::json();
+
+// 			if (JsonResult.is_discarded()) {
+// 				UE_LOG(LogTemp, Error, TEXT("JSON parsing failed: %s"), *str);
+// 				OutbIsSuccessful = false;
+// 				return nlohmann::json();
+// 			}
+// 			else {
+// 				UE_LOG(LogTemp, Warning, TEXT("Wants to ok"));
+
+// 				OutbIsSuccessful = true;
+// 				return JsonResult;
+// 			}
+
+// 		// JsonResult = nlohmann::json::parse(stdStr, errorHandler);
+// 		// JsonResult = nlohmann::json::parse(stdStr, nullptr, errorHandler);
+// 		// JsonResult = nlohmann::json::parse(stdStr.begin(), stdStr.end(), nullptr, errorHandler);
+// 	}
+
+// 	return nlohmann::json();
+// }
+
+// 
+// Private
+// 
+bool UMSGPackUtilitiesBPL::ConvertMsgpackToJsonObject(const TArray<uint8>& InBuffer, std::vector<uint8> OutDataVector, nlohmann::json& OutMsgpackJson)
+{
+	// Convert UE TArray to std::vector for msgpack conversion
+	std::vector<uint8> DataVector(InBuffer.GetData(), InBuffer.GetData() + InBuffer.Num());
+	OutDataVector = DataVector;
+
+	if (InBuffer.Num() > 0) {
+		// Convert DataVector (array of bytes) to msgpack
+		json JsonData = json::from_msgpack(DataVector);
+
+		// Check if JsonData is valid JSON
+		if (JsonData.is_null()) {
+			UE_LOG(LogTemp, Error, TEXT("Error: Invalid JSON, empty message or null received"));
+			return false;
+		}
+
+		OutMsgpackJson = JsonData;
+		return true;
+	}
+
+	UE_LOG(LogTemp, Error, TEXT("Received Empty Buffer"));
+
+	return false;
+}
+
+// 
+// Public
+// 
+void UMSGPackUtilitiesBPL::ConvertBufferToString(TArray<uint8> InBuffer, FString& OutString)
+{
+	OutString = FString::FromHexBlob(InBuffer.GetData(), InBuffer.Num());
+}
+
+
+bool UMSGPackUtilitiesBPL::GetPayloadMsgpackToJsonString(TArray<uint8> InMsgpackValue, FString& OutString)
+{
+	json JsonData;
+	std::vector<uint8> DataVector;
+	bool bIsSuccessful = ConvertMsgpackToJsonObject(InMsgpackValue, DataVector, JsonData);
+
+	if (!bIsSuccessful) {
+		UE_LOG(LogTemp, Error, TEXT("Error: Invalid JSON, empty message or null received"));
+		return false;
+	}
+
+	FString JsonString = FString(JsonData.dump().c_str());
+	OutString = JsonString;
+
+	return true;
+}
 
 
 bool UMSGPackUtilitiesBPL::GetPayloadMsgpackToJson(TArray<uint8> InMsgpackValue, FJsonObjectWrapper& OutJsonObject)
 {
-	// 1. Decode binary msgpack to msgpack
-	// 2. Attempt conversion to JSON then convert to JSON string
-	// 3. Attempt to deserialize JSON string to FJsonObject to support UE typesystem
-	std::vector<uint8> DataVector(InMsgpackValue.GetData(), InMsgpackValue.GetData() + InMsgpackValue.Num());
-	json JsonData = json::from_msgpack(DataVector);
-	FString JsonString = FString(JsonData.dump().c_str());
+	json JsonData;
+	std::vector<uint8> DataVector;
+	bool bIsSuccessful = ConvertMsgpackToJsonObject(InMsgpackValue, DataVector, JsonData);
 
+	if (!bIsSuccessful) {
+		UE_LOG(LogTemp, Error, TEXT("Failed to convert to Object! or received null"));
+		return false;
+	}
+
+	FString JsonString = FString(JsonData.dump().c_str());
+	TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonString);
 	TSharedPtr<FJsonObject> JsonObject;
 
-	// Returns bool - assign to JsonObject to check if successful
-	if (FJsonSerializer::Deserialize(TJsonReaderFactory<TCHAR>::Create(JsonString), JsonObject))
+	// Attempt conversion JsonString to valid UE Json Object
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject))
 	{
+		UE_LOG(LogTemp, Warning, TEXT("Deserialized Object!"));
+		OutJsonObject.JsonObject = JsonObject;
+
+		return true;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Failed to deserialize - %s"), *JsonString);
+
+	return false;
+}
+
+bool UMSGPackUtilitiesBPL::GetPayloadMsgpackToArray(TArray<uint8> InMsgpackValue, FJsonObjectWrapper& OutJsonObject)
+{
+	json JsonData;
+	std::vector<uint8> DataVector;
+	bool bIsSuccessful = ConvertMsgpackToJsonObject(InMsgpackValue, DataVector, JsonData);
+
+	if (!bIsSuccessful) {
+		UE_LOG(LogTemp, Error, TEXT("Failed to convert to Array! or received null"));
+		return false;
+	}
+
+	// Wrap into an object as UE does not support top-level arrays
+	FString JsonString = FString("{ \"array\": ") + FString(JsonData.dump().c_str()) + FString(" }");
+	TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonString);
+	TSharedPtr<FJsonObject> JsonObject;
+
+	if (FJsonSerializer::Deserialize(JsonReader, JsonObject)) {
+		UE_LOG(LogTemp, Warning, TEXT("Deserialized Top-level array wrapped in \"array\" object key!"));
 		OutJsonObject.JsonObject = JsonObject;
 		return true;
 	}
 
+	UE_LOG(LogTemp, Warning, TEXT("Failed to deserialize - %s"), *JsonString);
 
 	return false;
 }
--- Readme.md
+++ Readme.md
@@ -1,46 +1,53 @@
 # msgpack4ue - A simple Msgpack decoding plugin for Unreal Engine
 
-> msgpack4ue is a Unreal Engine plugin which enables decoding of msgpack into UE compliant JSON. 
+> msgpack4ue is a Unreal Engine plugin which enables decoding of msgpack into UE compliant JSON and primitives. 
 
-The primary usage of this plugin is to enable consuming msgpack data over MQTT. 
+The primary usage of this plugin is to enable consuming msgpack data over MQTT.  
+- [Tasks board](https://github.com/users/kyunwang/projects/1/views/1)
 
 | *The plugin has been developed and built with Unreal Engine 5.1.1*
 
 - [Features](#features)
 - [Supported platforms](#supported-platforms)
 - [Setup](#setup)
-- [Important](#important)
-
+- [Limitations](#limitations)
+- [Links](#links)
 
 ## Features
-- Decoding of binary Msgpack (`TArray<uint8>`) into UE compliant JSON
+- Decoding of msgpack format (`TArray<uint8>`) into
+  - UE Json
+  - Top-level array (wrapped in `{ "array": [...] }`)
+  - Empty/Null (Caught as failed)
+  - ~~Primitives (String, Integer, Floats, Booleans)~~ - Soon
+<!-- - Encoding UE JSON into msgpack format  -->
 
 ## Supported platforms
 Currently tested and confirmed to be working on:
 
 - Windows x64
-- Mac OS Ventura
+- Mac OS Ventura (Apple Silicon)
 
 ## Setup
+1. Download the plugin from [releases](https://github.com/kyunwang/msgpack4ue/releases)
+1. Unzip and place into your project's Plugin directory
+1. Enable the plugin in the Plugins window in the UE Editor
+1. The plugin contains an example blueprint which can be used to test the plugin namely, `ACT_BasicDecode`
+1. You might have to recompile the plugins when adding a/the MQTT plugin. It may crash the editor otherwise. (Tools -> Debug -> Modules -> msgpack4ue (Recompile))
+   1. Or delete the Binaries of both plugins and then recompile by opening the project
 
-- Download the plugin from releases
-- Unzip and place into your project's Plugin directory
-- Enable the plugin in the Plugins window in the UE Editor
-- The plugin contains an example blueprint which can be used to test the plugin namely, `ACT_BasicDecode`
+*P.S. Clone the repo for an example the plugin and MQTT. Code in BP ACT_MsgpackWithMQTT_Example*
 
-*P.S. Clone the repo for a simple example which uses MQTT implementation*
-
-## Important
+## Limitations
+- Input must be `TArray<uint8>`
+- Input must be valid JSON
+<!-- - or a pimitive type -->
 At time of writing:
-- The MQTT plugin the example uses is the MQTT Plugin from Unreal Engine
-- The msgpack type is exptected to be `TArray<uint8>` (As returned by the MQTT Plugin)
 
 
-## Known Issues
+<!-- ## Known Issues
 
 Regarding Epic's MQTT plugin not this plugin:
-- On MacOS - Crashes UE editor on exit play/simulate if the MQTT credentials/connection is incorrect/failed
+- On MacOS - Crashes UE editor on exit play/simulate if the MQTT credentials/connection is incorrect/failed -->
 
-
-## TODO
-- [ ] Add support for encoding JSON into msgpack
+## Links
+- [Msgpack](https://msgpack.org/)
